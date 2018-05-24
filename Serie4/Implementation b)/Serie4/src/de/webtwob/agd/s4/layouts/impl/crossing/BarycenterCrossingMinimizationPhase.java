c26e9c02df6ddc9af203b13ed137274ec2797c76
BB20101997 <bennet-blessmann@t-online.de>
2018-05-24 14:32:40 +0200

    replaced Function variable by a proper Function

--- Serie4/Implementation b)/Serie4/src/de/webtwob/agd/s4/layouts/impl/crossing/BarycenterCrossingMinimizationPhase.java
+++ Serie4/Implementation b)/Serie4/src/de/webtwob/agd/s4/layouts/impl/crossing/BarycenterCrossingMinimizationPhase.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.function.Function;
 
 import org.eclipse.elk.core.alg.ILayoutPhase;
 import org.eclipse.elk.core.alg.LayoutProcessorConfiguration;
@@ -23,109 +22,118 @@ import de.webtwob.agd.s4.layouts.options.LayerBasedMetaDataProvider;
 public class BarycenterCrossingMinimizationPhase implements ILayoutPhase<LayoutPhasesEnum, ElkNode> {
 
     // TODO Option for iterations
-    private static int ITERATIONS = 50;
+    private static int ITERATIONS = 51;
     
-    private static Function<Map<ElkNode,Double>,Comparator<ElkNode>> comparatorFactory = map ->{
-        return Comparator
-                .<ElkNode>comparingDouble(map::get)
-                .thenComparingDouble(n->n.getProperty(LayerBasedMetaDataProvider.OUTPUTS_POS_IN_LAYER));
-                };
+    private static Comparator<ElkNode> getComparator(Map<ElkNode,Double> map) {
+        return Comparator.<ElkNode> comparingDouble(map::get)
+                .thenComparingDouble(n -> n.getProperty(LayerBasedMetaDataProvider.OUTPUTS_POS_IN_LAYER));
+    }
 
     /**
      * assigns their Position in their layer based on their barycenter in the list.
      */
     @Override
     public void process(ElkNode graph, IElkProgressMonitor progressMonitor) {
-        Random r = new Random(42007); //Seed
-        
+        Random r = new Random(42007); // Seed
+
         Map<Integer, List<ElkNode>> layers = Util.getLayers(graph);
-        ArrayList<List<ElkNode>> permutations = new ArrayList<List<ElkNode>>();  // TODO Option for how many permutations 
-        
-        
-        for (int i = 0; i< 10; i++) { // TODO Option for how many permutations 
-        giveValuesFirst(layers.get(0), r);
-        // More than 1 Sweep
-        for (int j = 0; j < ITERATIONS; j++) {
-           if (j%1==0) {
-               // Forwards
-               downSweep(layers);
-           } else {
-               // Backwards
-               upSweep(layers);
-           }
-        }
-        
+        ArrayList<List<ElkNode>> permutations = new ArrayList<List<ElkNode>>(); // TODO Option for how many permutations
+
+        for (int i = 0; i < 10; i++) { // TODO Option for how many permutations
+            giveValuesFirst(layers.get(0), r);
+            // More than 1 Sweep
+            for (int j = 0; j < ITERATIONS; j++) {
+                if (j % 1 == 0) {
+                    // Forwards
+                    downSweep(layers);
+                } else {
+                    // Backwards
+                    upSweep(layers);
+                }
+            }
+
         }
     }
-    /**Sweeps from top to bottom
+
+    /**
+     * Sweeps from top to bottom
      * 
-     * @param layers The Layers of the Graph
+     * @param layers
+     *            The Layers of the Graph
      */
     private void downSweep(Map<Integer, List<ElkNode>> layers) {
+        Map<ElkNode, Double> barycenterMap = new HashMap<>();
+        Comparator<ElkNode> cmp = getComparator(barycenterMap);
+        
         for (int i = 1; i < layers.size() - 2; i++) {
             List<ElkNode> lower = layers.get(i);
 
-            Map<ElkNode,Double> barycenterMap = new HashMap<>();
-            
             double max = 1;
-            
-            for (ElkNode n : lower) {
-                
-                //calculate barycenter of current node
-                double barycenterOfNode = n.getIncomingEdges().stream()
-                    .map(e->Util.getSource(e))
-                    .mapToInt(node -> node.getProperty(LayerBasedLayoutMetadata.OUTPUTS_POS_IN_LAYER))
-                    .average()
-                    .orElse(max);
 
+            //calculate barycenter for each node
+            for (ElkNode n : lower) {
+
+                // calculate barycenter of current node
+                double barycenterOfNode = n.getIncomingEdges().stream().map(e -> Util.getSource(e))
+                        .mapToInt(node -> node.getProperty(LayerBasedLayoutMetadata.OUTPUTS_POS_IN_LAYER)).average()
+                        .orElse(max);
+
+                //update barycenter in the map
                 barycenterMap.put(n, barycenterOfNode);
-                //TODO is it correct that max should normally be increased by the Nodes barycenter and not set to it?
+                
                 max += n.getIncomingEdges().isEmpty() ? 1 : barycenterOfNode;
             }
-            
+
+            //make a 
             List<ElkNode> tmp = new ArrayList<>(lower);
-            tmp.sort(comparatorFactory.apply(barycenterMap));
-            
-            lower.forEach(n->n.setProperty(LayerBasedMetaDataProvider.OUTPUTS_POS_IN_LAYER, tmp.indexOf(n)));
-       
+            tmp.sort(cmp);
+
+            //update Position in layer
+            lower.forEach(n -> n.setProperty(LayerBasedMetaDataProvider.OUTPUTS_POS_IN_LAYER, tmp.indexOf(n)));
+
         }
     }
 
-    /**Sweeps from bottom to top
+    /**
+     * Sweeps from bottom to top
      * 
-     * @param layers The Layers of the Graph
+     * @param layers
+     *            The Layers of the Graph
      */
     private void upSweep(Map<Integer, List<ElkNode>> layers) {
+        Map<ElkNode, Double> barycenterMap = new HashMap<>();
+        Comparator<ElkNode> cmp = getComparator(barycenterMap);
+        
         for (int i = layers.size() - 2; i >= 0; i--) {
-            
+
             List<ElkNode> upper = layers.get(i);
             
-            Map<ElkNode,Double> barycenterMap = new HashMap<>();
-            
             double max = 1;
-            
+
+            //calculate barycenter for each node
             for (ElkNode n : upper) {
-                
-                //calculate barycenter of current node
-                double barycenterOfNode = n.getOutgoingEdges().stream()
-                    .map(e->Util.getTarget(e))
-                    .mapToInt(node -> node.getProperty(LayerBasedLayoutMetadata.OUTPUTS_POS_IN_LAYER))
-                    .average()
-                    .orElse(max);
-                
+
+                // calculate barycenter of current node
+                double barycenterOfNode = n.getOutgoingEdges().stream().map(e -> Util.getTarget(e))
+                        .mapToInt(node -> node.getProperty(LayerBasedLayoutMetadata.OUTPUTS_POS_IN_LAYER)).average()
+                        .orElse(max);
+
+
+                //update barycenter in the map
                 barycenterMap.put(n, barycenterOfNode);
-                
-                max += n.getOutgoingEdges().isEmpty() ? 1 :  barycenterOfNode;
+
+                max += n.getOutgoingEdges().isEmpty() ? 1 : barycenterOfNode;
             }
-            
+
             List<ElkNode> tmp = new ArrayList<>(upper);
-            tmp.sort(comparatorFactory.apply(barycenterMap));
-            
-            upper.forEach(n->n.setProperty(LayerBasedMetaDataProvider.OUTPUTS_POS_IN_LAYER, tmp.indexOf(n)));
-            
+            tmp.sort(cmp);
+
+
+            //update Position in layer
+            upper.forEach(n -> n.setProperty(LayerBasedMetaDataProvider.OUTPUTS_POS_IN_LAYER, tmp.indexOf(n)));
+
         }
     }
-    
 
     private Map<ElkNode, Integer> giveValuesFirst(List<ElkNode> first, Random r) {
         Collections.shuffle(first, r);
